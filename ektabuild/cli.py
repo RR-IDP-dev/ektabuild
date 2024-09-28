c597654bdc131736310fdfcd22143c6a68048c61
Rajesh Radhakrishnan <enthoughts@gmail.com>
2024-09-27 19:13:28 -0700

    first commit

---
+++ ektabuild/cli.py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------------------------
+#  Copyright (c) 2024 Rajesh Radhakrishnan
+#
+#  This file is part of the Ektabuild project, which is licensed under the MIT License.
+#  See the LICENSE file in the project root for more information.
+# -----------------------------------------------------------------------------
+
